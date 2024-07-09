09b9ebbff1812fcfd1e1edb8af4f39f0fc491d13
Isaac <39535421+I2ICreations@users.noreply.github.com>
2024-07-09 19:29:58 +0100

    1) I have added a list of words that could be used instead of just keys. 2) I have created a rand word selector that will choose a word at random from the words list.

--- Hangman/Hangman.cpp
+++ Hangman/Hangman.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
 
 using namespace std;
 
 int main()
 {
+    vector<string> words = { "apple", "banana", "cherry", "dragonfruit", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon",
+                            "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli", "watermelon" }; //A list of random words that could be chosen for the guessing game
 
-    string word = "keys";
+    srand(static_cast<unsigned int>(time(0)));  //Seed the random number generator 
+
+    string word = words[rand() % words.size()]; //Randomly select a word from the word list 
     string displayed_word(word.length(), '_');
     char guess;
 
